91b83c407d1c30b690f83fddc899a8531c7b3e20
Lloyd Hilsgen <hilsg008@multivac.morris.umn.edu>
2022-11-22 12:44:52 -0600

    Added 3rd arg to TestManyClients to assure that each time it's run it references its own temp files

--- src/TestManyClients.sh
+++ src/TestManyClients.sh
@@ -6,15 +6,13 @@
     for (( i=0; i<$numCalls; i++ ))
     do
         echo "Doing run $i"
-        java echoserver.EchoClient < $bigFile > "/tmp/test$i" 
+        java echoserver.EchoClient < $bigFile > "/tmp/test$i$3" 
     done
     echo "Now waiting for all the processes to terminate"
-    # `date` will output the date *and time* so you can see how long
-    # you had to wait for all the processes to finish.
     wait
     echo "Done waiting; all processes are finished"
     for (( i=0; i<$numCalls; i++ ))
     do
-        diff -s $2 "/tmp/test$i"
+        diff -s $2 "/tmp/test$i$3"
     done
            
